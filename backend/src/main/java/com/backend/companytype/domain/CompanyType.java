97b4f25a4a1b80f5c58553a84f6d2bff8a1b1ebd
juanrojasaguilar <yo217@hotmail.es>
2024-10-08 11:19:10 -0500

    feat: :sparkles: company type domain

---
+++ backend/src/main/java/com/backend/companytype/domain/CompanyType.java
@@ -0,0 +1,25 @@
+package com.backend.companytype.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@Table(name="companies_types")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class CompanyType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    @Column(name = "id_company_type")
+    private Long id;
+
+    @Column(length = 255)
+    private String description;
+}
