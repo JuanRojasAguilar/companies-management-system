b4d1ba761b58201713d33e56efa3001a7a14cf5c
juanrojasaguilar <yo217@hotmail.es>
2024-10-11 12:51:05 -0500

    feat: :construction: user entity

--- backend/src/main/java/com/backend/user/domain/User.java
+++ backend/src/main/java/com/backend/user/domain/User.java
@@ -1,8 +1,14 @@
 package com.backend.user.domain;
 
+import java.sql.Date;
+
+import com.backend.franchise.domain.Franchise;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -16,7 +22,19 @@ public class User {
     @Id
     @Column(name = "user_id", unique = true, nullable = false, length = 40)
     @NonNull
+    @EqualsAndHashCode.Include
     private String id;
 
+    @Column(length = 50)
+    private String name;
 
+    @Column(length = 50, name = "last_name")
+    private String lastName;
+
+    @Column(name = "register_date", updatable = false, insertable = false)
+    private Date registerDate;
+
+    @ManyToOne
+    @JoinColumn(name = "franchise_id")
+    private Franchise franchise;
 }
